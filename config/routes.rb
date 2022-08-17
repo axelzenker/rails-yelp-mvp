63501f58fcfdf3cbb32e2c3e31b6ff789f82c9b3
Axel Zenker <axelzenker91@gmail.com>
2022-08-17 17:21:28 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'restaurants', to: 'restaurants#index', as: :list
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  post "restaurants", to: "restaurants#create"
+
+  get 'reviews', to: 'reviews#'
+end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+takumi = {name: "Takumi", address: "Hohenstaufenring 58, 50674 Köln", phone_number:"0221/34899171", category: "chinese"}
+chum_chai = {name: "Chum Chai", address: "Friesenwall 29, 50672 Köln", phone_number:"0221/79003798", category: "chinese"}
+
+[takumi, chum_chai].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
